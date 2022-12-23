d333fda8ebd5c323caa1e5335cdd3bb142c259fd
ELENAdragon11 <110730278+ELENAdragon11@users.noreply.github.com>
2022-12-23 17:45:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let del1 = document.querySelector('.del1');
+let del2 = document.querySelector('.del2');
+let del3 = document.querySelector('.del3');
+del1.addEventListener('click',() => {
+del1.remove();
+})
+del2.addEventListener('click',() => {
+    del2.remove();
+    })
+    del3.addEventListener('click',() => {
+        del3.remove();
+        })
